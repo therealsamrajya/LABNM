7f77ff5607a141399b43e32282673bbd68186e07
Samrajya <97937630+therealsamrajya@users.noreply.github.com>
2022-08-01 14:02:16 +0545

    Update NM2 NR.cpp

--- NM2 NR.cpp
+++ NM2 NR.cpp
@@ -2,19 +2,19 @@
 #include<math.h>
 using namespace std;
 
-#define f(x) 3*x*x-6*x+2 //equation
-#define g(x) 6*x-6  // derivative of the equation
+#define f(x) exp(-x)-3*x
+#define g(x) -exp(-x)-3
 
 int main()
 {
-    float xi,x,fi,gi,f=1,e=1;
+    double xi,x,fi,gi,f=1,e=1;
     cout<< "enter the initial guess x0:";
     cin>>xi;
     while(e>=0.0001 && f!=0)
     {
         fi=f(xi);
         gi=g(xi);
-        x=xi-(fi/gi); //working formula
+        x=xi-(fi/gi); 
         f=f(x);
         e=fabs((x-xi)/x);
         xi=x;
